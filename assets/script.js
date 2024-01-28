d46dc203746760fa872b84c3bd3e0ad14fc19433
Snalepa <snalepa1998@gmail.com>
2024-01-27 18:38:55 -0600

    fetched required APISfor projects

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+var weatherAPI = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
+var apiKey = '77b6e16c240d6ac8e0f66b5dd65a3e77'
+
+geoURL =`https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${apiKey}`
+
+
+
+
